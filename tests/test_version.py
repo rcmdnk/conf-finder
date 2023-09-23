90c7a84ca6c1796e8b951ca9e1b4c2a25ecff476
rcmdnk <rcmdnk@gmail.com>
2023-09-23 10:27:52 +0900

    Initial commit

---
+++ tests/test_version.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+
+try:
+    import tomllib
+except ModuleNotFoundError:
+    import tomli as tomllib
+
+from python_template import __version__
+
+
+def test_version():
+    with open(Path(__file__).parents[1] / "pyproject.toml", "rb") as f:
+        version = tomllib.load(f)["tool"]["poetry"]["version"]
+    assert version == __version__
